9980ea00b0dd4dde270a62bb8d9cfe9d2c809370
dlmolloy97 <72402434+dlmolloy97@users.noreply.github.com>
2023-01-07 19:44:56 +0100

    Remove unneeded reactives.R module

--- R/reactives.R
+++
@@ -1,8 +0,0 @@
-#' @export
-grouping_neighbourhood_type <- function(direction) {
-  return(ifelse(direction=="Inbound","start_neighbourhood", "end_neighbourhood"))
-}
-
-selected_neighbourhood_type <- function(direction) {
-  return(ifelse(direction=="Inbound","end_neighbourhood", "start_neighbourhood"))
-}
